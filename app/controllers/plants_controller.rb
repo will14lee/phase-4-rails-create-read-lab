26a253a0133d4330df93544f1da95d81f3bb9505
will14lee <will14lee@gmail.com>
2021-12-01 00:42:28 -0500

    Completed Assignment

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,26 @@
+class PlantsController < ApplicationController
+    def index
+        plants = Plant.all
+        render json: plants
+    end
+    
+    
+    def create
+        plant = Plant.create(plant_params)
+        render json: plant, status: :created
+    end
+    
+    def show
+        plant = Plant.find_by(id: params[:id])
+        if plant
+            render json: plant
+        else
+          render json: { error: "Plant not found" }, status: :not_found
+        end
+    end
+    
+    private
+    def plant_params
+        params.permit(:name, :image, :price)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_12_01_053650) do
+
+  create_table "plants", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.decimal "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
